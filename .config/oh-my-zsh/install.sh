95534b077572480d1fe06d88327d0adf5faaadd9
efydd <efydd.artio@tuta.io>
2019-04-21 09:11:28 +0200

    add ohmyzsh install script, no need to copy all ohmyzsh repo

---
+++ .config/oh-my-zsh/install.sh
@@ -0,0 +1,4 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mv /home/$USER/.oh-my-zsh/ /home/$USER/.config/oh-my-zsh/
+git clone https://github.com/zsh-users/zsh-autosuggestions /home/$USER/.config/oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting /home/$USER/.config/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
