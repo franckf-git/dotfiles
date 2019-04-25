941bb20a294d7d76e07d19b64e11b4d139b6ddfb
efydd <efydd.artio@tuta.io>
2019-04-25 07:09:59 +0200

    individual shortcut for rofi autoselect

--- .config/scripts/launcher.sh
+++ .config/scripts/launcher.sh
@@ -2,59 +2,61 @@
 
 perso () {
 CHOIX=$(echo -e "\
- brave\n\
- check repo\n\
- chromium\n\
- google chrome\n\
- duckduckgo\n\
- firefox\n\
- meteo\n\
- neovim\n\
- newsboat\n\
- radio\n\
- ranger\n\
- read me\n\
- gitlab\n\
- sublime\n\
- surf\n\
- wikipedia\
-" | rofi -dmenu -l 16)
+ (B)rave\n\
+ (C)hromium\n\
+ (G)oogle chrome\n\
+ (F)irefox\n\
+ s(U)rf\n\
+ (D)uckduckgo\n\
+ neo(V)im\n\
+ rea(D) me\n\
+ (S)ublime\n\
+ check r(E)po\n\
+ git(L)ab\n\
+ (W)ikipedia\n\
+ newsboa(T)\n\
+ radi(O)\n\
+ (R)anger\n\
+ (M)eteo\n\
+-\n\
+-\
+" | rofi -dmenu -l 6)
     case "$CHOIX" in
-        " sublime"           ) /home/$USER/.sublime_text_3/sublime_text ; flatpak run com.sublimetext.three ;;
-        " ranger"            ) xfce4-terminal -T ranger -e 'ranger' ;;
-        " read me"           ) xfce4-terminal -T readme -e 'nvim -p /home/$USER/Unclear/readme.md /home/$USER/Unclear/path /home/$USER/Files/InProgress' ;;
-        " neovim"            ) xfce4-terminal -T neovim -e 'nvim' ;;
-        " newsboat"          ) xfce4-terminal -T rss -e "newsboat --config-file=/home/$USER/.config/newsboat/config --url-file=/home/$USER/.config/newsboat/urls --cache-file=/home/$USER/.config/newsboat/cache.db" ;;
-        " radio"             ) urxvt -title radio -e sh -ic 'mpv --no-video https://chai5she.cdn.dvmr.fr/fip-webradio1.mp3?ID=radiofrance' ;;
-        " meteo"             ) urxvt -e sh -ic 'curl --header "Accept-Language: fr" wttr.in/poitiers && bash' ;;
-        " check repo"         ) urxvt -e sh -ic 'bash /home/$USER/.config/scripts/check_repository.sh' ;;
-        " brave"             ) brave-browser-stable ;;
-        " chromium"          ) chromium-browser --incognito ;;
-        " google chrome"     ) google-chrome ;;
-        " firefox"           ) firefox --private-window ;;
-        " surf"              ) surf -u "Mozilla/5.0 (Windows NT 10.0;) Gecko Firefox/66.0" -dgNpis -a a file:///home/$USER/.config/bookmarks/home.html ;;
-        " gitlab"            ) surf -u "Mozilla/5.0 (Windows NT 10.0;) Gecko Firefox/66.0" -dgNpiS -a a https://framagit.org/efydd ;;
-        " wikipedia"        ) surf -u "Mozilla/5.0 (Windows NT 10.0;) Gecko Firefox/66.0" -dgNpIs -a a https://fr.wikipedia.org ;;
-        " duckduckgo"        ) surf -u "Mozilla/5.0 (Windows NT 10.0;) Gecko Firefox/66.0" -dgNpis -a a https://duckduckgo.com/ ;;
+        " (S)ublime"           ) /home/$USER/.sublime_text_3/sublime_text ; flatpak run com.sublimetext.three ;;
+        " (R)anger"            ) xfce4-terminal -T ranger -e 'ranger' ;;
+        " rea(D) me"           ) xfce4-terminal -T readme -e 'nvim -p /home/$USER/Unclear/readme.md /home/$USER/Unclear/path /home/$USER/Files/InProgress' ;;
+        " neo(V)im"            ) xfce4-terminal -T neovim -e 'nvim' ;;
+        " newsboa(T)"          ) xfce4-terminal -T rss -e "newsboat --config-file=/home/$USER/.config/newsboat/config --url-file=/home/$USER/.config/newsboat/urls --cache-file=/home/$USER/.config/newsboat/cache.db" ;;
+        " radi(O)"             ) urxvt -title radio -e sh -ic 'mpv --no-video https://chai5she.cdn.dvmr.fr/fip-webradio1.mp3?ID=radiofrance' ;;
+        " (M)eteo"             ) urxvt -e sh -ic 'curl --header "Accept-Language: fr" wttr.in/poitiers && bash' ;;
+        " check r(E)po"         ) urxvt -e sh -ic 'bash /home/$USER/.config/scripts/check_repository.sh' ;;
+        " (B)rave"             ) brave-browser-stable ;;
+        " (C)hromium"          ) chromium-browser --incognito ;;
+        " (G)oogle chrome"     ) google-chrome ;;
+        " (F)irefox"           ) firefox --private-window ;;
+        " s(U)rf"              ) surf -u "Mozilla/5.0 (Windows NT 10.0;) Gecko Firefox/66.0" -dgNpis -a a file:///home/$USER/.config/bookmarks/home.html ;;
+        " git(L)ab"            ) surf -u "Mozilla/5.0 (Windows NT 10.0;) Gecko Firefox/66.0" -dgNpiS -a a https://framagit.org/efydd ;;
+        " (W)ikipedia"        ) surf -u "Mozilla/5.0 (Windows NT 10.0;) Gecko Firefox/66.0" -dgNpIs -a a https://fr.wikipedia.org ;;
+        " (D)uckduckgo"        ) surf -u "Mozilla/5.0 (Windows NT 10.0;) Gecko Firefox/66.0" -dgNpis -a a https://duckduckgo.com/ ;;
     esac
 }
 
 system () {
 CHOIX=$(echo -e "\
- shutdown\n\
- reboot\n\
- lock\n\
- exit i3\n\
- restart i3\n\
+ (S)hutdown\n\
+ (R)eboot\n\
+ (L)ock\n\
+ (E)xit i3\n\
+ restart (I)3\n\
   touchpad on\n\
   touchpad off\
 " | rofi -dmenu -l 5)
     case "$CHOIX" in
-        " shutdown"        ) systemctl poweroff --ignore-inhibitors ;;
-        " reboot"          ) systemctl reboot --ignore-inhibitors ;;
-        " lock"            ) i3lock --nofork --color=272822 --tiling --image=/home/$USER/.config/themes/lock.png ;;
-        " exit i3"         ) i3-msg exit ;;
-        " restart i3"      ) i3-msg restart ;;
+        " (S)hutdown"        ) systemctl poweroff --ignore-inhibitors ;;
+        " (R)eboot"          ) systemctl reboot --ignore-inhibitors ;;
+        " (L)ock"            ) i3lock --nofork --color=272822 --tiling --image=/home/$USER/.config/themes/lock.png ;;
+        " (E)xit i3"         ) i3-msg exit ;;
+        " restart (I)3"      ) i3-msg restart ;;
         "  touchpad on"   ) xinput enable 11 && notify-send " the touchpad is enable" ;;
         "  touchpad off"  ) xinput disable 11 && notify-send " the touchpad is disable" ;;
     esac
@@ -62,16 +64,16 @@ CHOIX=$(echo -e "\
 
 rotate () {
 CHOIX=$(echo -e "\
- normal\n\
- left\n\
- right\n\
- inverted\
-" | rofi -dmenu -l 4)
+ (N)ormal\n\
+ (L)eft\n\
+ (R)ight\n\
+ (I)nverted\
+" | rofi -dmenu -l 2)
     case "$CHOIX" in
-        " normal"      ) xrandr --output $screen0 --rotate normal ;;
-        " left"        ) xrandr --output $screen0 --rotate left ;;
-        " right"       ) xrandr --output $screen0 --rotate right ;;
-        " inverted"    ) xrandr --output $screen0 --rotate inverted ;;
+        " (N)ormal"      ) xrandr --output $screen0 --rotate normal ;;
+        " (L)eft"        ) xrandr --output $screen0 --rotate left ;;
+        " (R)ight"       ) xrandr --output $screen0 --rotate right ;;
+        " (I)nverted"    ) xrandr --output $screen0 --rotate inverted ;;
     esac
 }
 
