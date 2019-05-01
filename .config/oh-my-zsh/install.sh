80c4e9dc3b460d4c55e38412a8f6fbdfa78efa2d
efydd <efydd.artio@tuta.io>
2019-05-01 11:37:44 +0200

    add powerlevel9k to custom theme

--- .config/oh-my-zsh/install.sh
+++ .config/oh-my-zsh/install.sh
@@ -2,3 +2,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 mv /home/$USER/.oh-my-zsh/ /home/$USER/.config/oh-my-zsh/
 git clone https://github.com/zsh-users/zsh-autosuggestions /home/$USER/.config/oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting /home/$USER/.config/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/bhilburn/powerlevel9k.git /home/$USER/.config/oh-my-zsh/custom/themes/powerlevel9k
