2acdc4d6c61794d903fb74a070c0c91140404dbb
efydd <efydd.artio@tuta.io>
2019-05-15 18:21:37 +0200

    set the i3 bar to minimal

--- .config/scripts/i3title_status.sh
+++ .config/scripts/i3title_status.sh
@@ -4,5 +4,5 @@ do
     read line
     id=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
     name=$(xprop -id $id | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
-    echo "$name    $line" || exit 1
+    echo "$name                                                                                                                $line    " || exit 1
 done
