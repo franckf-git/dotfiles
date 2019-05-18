a459fb399c1c31019939b529faeaf458cf17f304
efydd <efydd.artio@tuta.io>
2019-05-18 08:05:33 +0200

    fix auto colorscheme ?

--- .config/scripts/startup.sh
+++ .config/scripts/startup.sh
@@ -1,8 +1,5 @@
 #! /bin/sh
 
-# colorscheme
-xrdb -load ~/.Xdefaults &
-i3-msg reload &
 # background and mouse
 feh --bg-center /home/$USER/.config/themes/wallpaper.png &
 conky --config=/home/$USER/.config/conky/conkyrc &
@@ -12,3 +9,6 @@ urxvt -e sh -ic 'bash /home/$USER/.config/scripts/check_repository.sh' &
 xfce4-terminal -T rss -e "newsboat --config-file=/home/$USER/.config/newsboat/config --url-file=/home/$USER/.config/newsboat/urls --cache-file=/home/$USER/.config/newsboat/cache.db" &
 xfce4-terminal -T ranger -e 'ranger' &
 firefox --private-window file:///home/$USER/.config/bookmarks/home.html &
+# colorscheme
+xrdb -load ~/.Xdefaults &
+i3-msg reload &
