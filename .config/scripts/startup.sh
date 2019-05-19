f1877b6d23608ded9c20be1d79bf942de99f5eb0
efydd <efydd.artio@tuta.io>
2019-05-19 06:52:11 +0200

    fix reload colorscheme i3 and fix some terminals lauch

--- .config/scripts/startup.sh
+++ .config/scripts/startup.sh
@@ -2,13 +2,14 @@
 
 # background and mouse
 feh --bg-center /home/$USER/.config/themes/wallpaper.png &
-conky --config=/home/$USER/.config/conky/conkyrc &
+conky  --config=/home/$USER/.config/conky/conky.conf &
 unclutter -idle 1 &
 # apps
-urxvt -e sh -ic 'bash /home/$USER/.config/scripts/check_repository.sh' &
-xfce4-terminal -T rss -e "newsboat --config-file=/home/$USER/.config/newsboat/config --url-file=/home/$USER/.config/newsboat/urls --cache-file=/home/$USER/.config/newsboat/cache.db" &
-xfce4-terminal -T ranger -e 'ranger' &
+xfce4-terminal -T term -e   "bash /home/$USER/.config/scripts/check_repository.sh" &
+xfce4-terminal -T rss -e    "newsboat --config-file=/home/$USER/.config/newsboat/config --url-file=/home/$USER/.config/newsboat/urls --cache-file=/home/$USER/.config/newsboat/cache.db" &
+xfce4-terminal -T ranger -e "ranger" &
 firefox --private-window file:///home/$USER/.config/bookmarks/home.html &
 # colorscheme
+sleep 3s
 xrdb -load ~/.Xdefaults &
 i3-msg reload &
