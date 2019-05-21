dc5a660cba5d80de3716d0e513c9e431a83e86b3
efydd <efydd.artio@tuta.io>
2019-05-21 06:20:45 +0200

    use env variables

--- .config/scripts/launcher.sh
+++ .config/scripts/launcher.sh
@@ -27,25 +27,25 @@ CHOICES=$(echo -e "\
 -\
 " | rofi -dmenu -l 8 -p "  ")
     case "$CHOICES" in
-        " radio"          ) urxvt -title radio -e sh -ic 'mpv --no-video https://chai5she.cdn.dvmr.fr/fip-webradio1.mp3?ID=radiofrance' ;;
-        " duckduckgo"     ) surf -u "Mozilla/5.0 (Windows NT 10.0;) Gecko Firefox/66.0" -dgNpis -a a https://duckduckgo.com/ ;;
+        " radio"          ) $LITETERMINAL -title radio -e sh -ic 'mpv --no-video https://chai5she.cdn.dvmr.fr/fip-webradio1.mp3?ID=radiofrance' ;;
+        " duckduckgo"     ) $BROWSER https://duckduckgo.com/?q=&ka=monospace&kh=1&kj=p&kt=monospace&k1=-1&kae=d&ia=answer ;;
         " windows"        ) rofi -modi window -show window -sidebar-mode -show-icons -drun-icon-theme  ;;
         " applications"   ) rofi -modi drun -show drun -sidebar-mode -show-icons -drun-icon-theme ;;
         " shutdown"       ) systemctl poweroff --ignore-inhibitors ;;
         " lock"           ) i3lock --nofork --color=2a1d17 --tiling --image=/home/$USER/.config/themes/lock.png ;;
         " exit i3"        ) i3-msg exit ;;
         " reboot"         ) systemctl reboot --ignore-inhibitors ;;
-        " ranger"         ) xfce4-terminal -T ranger -e 'ranger' ;;
-        " newsboat"       ) xfce4-terminal -T rss -e "newsboat --config-file=/home/$USER/.config/newsboat/config --url-file=/home/$USER/.config/newsboat/urls --cache-file=/home/$USER/.config/newsboat/cache.db" ;;
-        " meteo"          ) urxvt -e sh -ic 'curl --header "Accept-Language: fr" wttr.in/poitiers && bash' ;;
+        " ranger"         ) $TERMINAL -T $FILE -e $FILE ;;
+        " newsboat"       ) $TERMINAL -T rss -e "newsboat --config-file=/home/$USER/.config/newsboat/config --url-file=/home/$USER/.config/newsboat/urls --cache-file=/home/$USER/.config/newsboat/cache.db" ;;
+        " meteo"          ) $LITETERMINAL -e sh -ic 'curl --header "Accept-Language: fr" wttr.in/poitiers && bash' ;;
         " layout"         ) i3-msg "layout splitv ; move left ; resize grow right 20 px or 20 ppt" ;;
-        " sublime"        ) /home/$USER/.sublime_text_3/sublime_text ; flatpak run com.sublimetext.three ;;
-        " check repo"     ) xfce4-terminal -T term -e   "bash /home/$USER/.config/scripts/check_repository.sh" ;;
-        " read me"        ) xfce4-terminal -T readme -e 'nvim -p Unclear/readme.md Unclear/path Files/InProgress' ;;
-        " surf"           ) surf -u "Mozilla/5.0 (Windows NT 10.0;) Gecko Firefox/66.0" -dgNpiS -a a file:///home/$USER/.config/bookmarks/home.html ;;
+        " sublime"        ) $BIGEDITOR ;;
+        " check repo"     ) $TERMINAL -T term -e   "bash /home/$USER/.config/scripts/check_repository.sh" ;;
+        " read me"        ) $TERMINAL -T readme -e "nvim -p Unclear/readme.md Unclear/path Files/InProgress" ;;
+        " surf"           ) $BROWSER file:///home/$USER/.config/bookmarks/home.html ;;
         " restart i3"     ) i3-msg restart ;;
         " chromium"       ) chromium-browser --incognito ;;
         " firefox"        ) firefox --private-window ;;
         " network"        ) nm-connection-editor ;;
-        " gitlab"         ) surf -u "Mozilla/5.0 (Windows NT 10.0;) Gecko Firefox/66.0" -dgNpiS -a a https://framagit.org/efydd ;;
+        " gitlab"         ) $BROWSER a https://framagit.org/efydd ;;
     esac
