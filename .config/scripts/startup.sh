2ed5dd3467ac7f1f44e8d9905790c808bc651fe0
efydd <efydd.artio@tuta.io>
2019-05-22 07:04:58 +0200

    XDG env variables

--- .config/scripts/startup.sh
+++ .config/scripts/startup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # background and mouse
-feh --bg-center /home/$USER/.config/themes/wallpaper.png &
-conky  --config=/home/$USER/.config/conky/conky.conf &
+feh --bg-center $XDG_CONFIG_HOME/themes/wallpaper.png &
+conky  --config=$XDG_CONFIG_HOME/conky/conky.conf &
 unclutter -idle 1 &
 # apps
-xfce4-terminal -T term -e   "bash /home/$USER/.config/scripts/check_repository.sh" &
-xfce4-terminal -T rss -e    "newsboat --config-file=/home/$USER/.config/newsboat/config --url-file=/home/$USER/.config/newsboat/urls --cache-file=/home/$USER/.config/newsboat/cache.db" &
+xfce4-terminal -T term -e   "bash $XDG_CONFIG_HOME/scripts/check_repository.sh" &
+xfce4-terminal -T rss -e    "newsboat --config-file=$XDG_CONFIG_HOME/newsboat/config --url-file=$XDG_CONFIG_HOME/newsboat/urls --cache-file=$XDG_CONFIG_HOME/newsboat/cache.db" &
 xfce4-terminal -T ranger -e "ranger" &
-firefox --private-window file:///home/$USER/.config/bookmarks/home.html &
+firefox --private-window file://$XDG_CONFIG_HOME/bookmarks/home.html &
 # colorscheme
-sleep 2s
+sleep 3s
 xrdb -load ~/.Xdefaults &
 i3-msg reload &
