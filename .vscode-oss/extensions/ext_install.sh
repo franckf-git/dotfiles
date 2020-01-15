22683a14d0e86c878a590854bd8a9c48c63db15e
franckf <efydd.artio@tuta.io>
2020-01-15 06:04:45 +0100

    install vscode extension with bash

---
+++ .vscode-oss/extensions/ext_install.sh
@@ -0,0 +1,16 @@
+codium --install-extension aaron-bond.better-comments
+codium --install-extension amandeepmittal.expressjs
+codium --install-extension capaj.vscode-standardjs-snippets
+codium --install-extension chenxsan.vscode-standardjs
+codium --install-extension christian-kohler.npm-intellisense
+codium --install-extension christian-kohler.path-intellisense
+codium --install-extension digitalbrainstem.javascript-ejs-support
+codium --install-extension eg2.vscode-npm-script
+codium --install-extension formulahendry.auto-rename-tag
+codium --install-extension hookyqr.beautify
+codium --install-extension redhat.vscode-yaml
+codium --install-extension tyriar.sort-lines
+codium --install-extension visualstudioexptteam.vscodeintellicode
+codium --install-extension vscode-icons-team.vscode-icons
+codium --install-extension vscoss.vscode-ansible
+codium --install-extension xabikos.javascriptsnippets
