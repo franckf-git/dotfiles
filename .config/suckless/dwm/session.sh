90192090dea48db65b0daa31a1275356746d6714
franckf <efydd.artio@tuta.io>
2020-05-24 09:16:09 +0200

    other way to get battery %

--- .config/suckless/dwm/session.sh
+++ .config/suckless/dwm/session.sh
@@ -2,6 +2,7 @@
 while true; do
    batterie=$(upower -i /sys/class/power_supply/BAT0)
    #upower -i /org/freedesktop/UPower/devices/battery_BAT0
+   #cat /sys/class/power_supply/BAT0/capacity
    wifi=$(ip a | grep wlp0 | grep state | cut -d " " -f9)
    date=$( date +"%F %R" )
    xsetroot -name "$date"
