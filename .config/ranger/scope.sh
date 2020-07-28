289e0db02fcf5ff40c48f5f8f58d88a60ce7a0fb
franckf <efydd.artio@tuta.io>
2020-07-28 07:44:37 +0200

    disable w3m preview for html - to see code

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -79,9 +79,9 @@ handle_extension() {
         # HTML
         htm|html|xhtml)
             # Preview as text conversion
-            w3m -dump "${FILE_PATH}" && exit 5
-            lynx -dump -- "${FILE_PATH}" && exit 5
-            elinks -dump "${FILE_PATH}" && exit 5
+            #w3m -dump "${FILE_PATH}" && exit 5
+            #lynx -dump -- "${FILE_PATH}" && exit 5
+            #elinks -dump "${FILE_PATH}" && exit 5
             ;; # Continue with next handler on failure
     esac
 }
