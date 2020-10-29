123335faf7eb74cc170d16fda6b85c684d48e369
franckf <efydd.artio@tuta.io>
2020-10-29 07:20:56 +0100

    [FIX] fix errors to grab rss - model with persistance

--- .config/newsboat/anime-ultime-rss.sh
+++ .config/newsboat/anime-ultime-rss.sh
@@ -1,33 +1,57 @@
-#! /bin/sh
+#! /bin/bash
 # check new entry at anime-ultime.net and convert to rss
 # in ~/.config/newsboat/urls add :
 # exec:~/.config/newsboat/anime-ultime-rss.sh
+
 baseurl="http://www.anime-ultime.net"
 url="$baseurl/index-0-1"
-entries=$(curl $url | grep left | cut -d'"' -f6 | sed '$ d')
-# -f11 for title
+today=$(date +"%a %d %b %Y 01:00:00 +0000")
 
+cd ~/.config/newsboat
+
+# get the actuals published animes
+curl $url | sed -n '/<table/,/table>/p' > today-anime.html
+
+news=$(diff today-anime.html yesterday-anime.html)
+
+entries=$(echo "$news" | grep "left")
+
+# build rss XML
+IFS=$'\n'
 cat <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0">
 
 <channel>
-<title>anime ultime</title>
+<title>anime-ultime</title>
 <link>$url</link>
 <description>animes</description>
 
 EOF
 {
-for news in $entries ; do
+for entry in $entries
+do
+    titles=$(echo $entry | cut -d'/' -f3 | cut -d'"' -f1)
+    for title in $titles
+    do
+        tag=$(echo "$news" | sed -n "/$title/,/charger/p")
+        link=$(echo "$tag" | grep "left" | cut -d'"' -f6)
+        epnumber=$(echo "$tag" | sed '2!d' | cut -d'>' -f2 | sed '1s/....$//' )
         echo "    <item>"
-        echo "      <title>$news</title>"
-        echo "      <link>$baseurl/$news</link>"
+        echo "      <title>$title $epnumber</title>"
+        echo "      <link>$baseurl/$link</link>"
+        echo "      <pubDate>$today</puDate>"
         echo "    </item>"
     done
-    echo "  </channel>"
-    echo "</rss>"
+done
+echo "  </channel>"
+echo "</rss>"
 }
 
-# TODO
-# add genre (anime, drama, ...)
-# add episode number
+unset IFS
+
+rm yesterday-anime.html
+mv today-anime.html yesterday-anime.html
+
+cd ~/
+
