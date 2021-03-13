529fa9f13300e966daa70689a6a3c63442b9ae02
franckf <>
2021-03-13 06:43:51 +0100

    fix: fiber! full download now

--- .config/newsboat/dl.sh
+++ .config/newsboat/dl.sh
@@ -6,7 +6,7 @@ domain=$(echo $1 | cut -d"." -f2)
 
 if [[ $domain == "youtube" ]]
 then
-    cd ~/Vidéos  && youtube-dl --write-auto-sub --add-metadata -ic -f 'bestvideo[height<=720]+bestaudio/best[height<=720]' $url
+    cd ~/Vidéos  && youtube-dl --write-auto-sub --add-metadata -ic $url
 else
     cd ~/Musique && wget $url
 fi
